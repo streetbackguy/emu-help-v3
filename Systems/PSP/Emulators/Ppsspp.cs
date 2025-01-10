1b473bcab3a65bb330f4805e003018ef5ab5c7de
streetbackguy <65618875+streetbackguy@users.noreply.github.com>
2025-01-10 18:29:26 +0000

    Create Ppsspp.cs

---
+++ Systems/PSP/Emulators/Ppsspp.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Linq;
+using EmuHelp.Logging;
+using JHelper.Common.ProcessInterop;
+
+namespace EmuHelp.Systems.PSP.Emulators;
+
+internal class Ppsspp : PSPEmulator
+{
+    private IntPtr addr_base;
+
+    internal Ppsspp()
+        : base()
+    {
+        Log.Info("  => Attached to emulator: PPSSPP");
+    }
+
+    public override bool FindRAM(ProcessMemory _process)
+    {
+        if (_process.Is64Bit)
+        {
+            addr_base = _process.Scan(new MemoryScanPattern(3, "48 8B ?? ?? ?? ?? ?? 25 F0 3F 00 00") { OnFound = addr => addr + 0x4 + _process.Read<int>(addr) });
+            if (addr_base == IntPtr.Zero)
+                return false;
+
+            if (!_process.Read(addr_base, out IntPtr ptr))
+                return false;
+            RamBase = ptr;
+        }
+        else
+        {
+            addr_base = new MemoryScanPattern[]
+            {
+                new(2, "8B ?? ?? ?? ?? ?? 25 F0 3F 00 00") { OnFound = _process.ReadPointer },
+                new(2, "8B ?? ?? ?? ?? ?? 81 ?? F0 3F 00 00") { OnFound = _process.ReadPointer },
+            }
+            .Select(_process.Scan).FirstOrDefault(addr => addr != IntPtr.Zero);
+
+            if (addr_base == IntPtr.Zero)
+                return false;
+
+            if (!_process.Read(addr_base, out IntPtr ptr))
+                return false;
+            RamBase = ptr;
+        }
+
+        if (RamBase != IntPtr.Zero)
+            Log.Info($"  => RAM address found at 0x{RamBase.ToString("X")}");
+        return true;
+    }
+
+    public override bool KeepAlive(ProcessMemory process)
+    {
+        if (!process.Read(addr_base, out IntPtr ptr))
+            return false;
+        RamBase = ptr;
+
+        return true;
+    }
+}
