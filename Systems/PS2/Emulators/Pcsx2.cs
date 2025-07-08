174c937915b4bb1337f609fc0485bcf86e4e3bd7
Jujstme <just.tribe@gmail.com>
2025-07-08 21:32:37 +0200

    Fixed PCSX2 scanning code

--- Systems/PS2/Emulators/Pcsx2.cs
+++ Systems/PS2/Emulators/Pcsx2.cs
@@ -20,26 +20,27 @@ internal class Pcsx2 : PS2Emulator
         if (_process.MainModule.Symbols.TryGetValue("EEmem", out IntPtr symbol))
         {
             addr_base = symbol;
-            return true;
-        }
-
-        if (_process.Is64Bit)
-        {
-            addr_base = _process.Scan(new ScanPattern(3, "48 8B ?? ?? ?? ?? ?? 25 F0 3F 00 00") { OnFound = addr => addr + 0x4 + _process.Read<int>(addr) });
-            if (addr_base == IntPtr.Zero)
-                return false;
         }
         else
         {
-            addr_base = new ScanPattern[]
+            if (_process.Is64Bit)
             {
+                addr_base = _process.Scan(new ScanPattern(3, "48 8B ?? ?? ?? ?? ?? 25 F0 3F 00 00") { OnFound = addr => addr + 0x4 + _process.Read<int>(addr) });
+                if (addr_base == IntPtr.Zero)
+                    return false;
+            }
+            else
+            {
+                addr_base = new ScanPattern[]
+                {
                 new(2, "8B ?? ?? ?? ?? ?? 25 F0 3F 00 00") { OnFound = _process.ReadPointer },
                 new(2, "8B ?? ?? ?? ?? ?? 81 ?? F0 3F 00 00") { OnFound = _process.ReadPointer },
-            }
-            .Select(_process.Scan).FirstOrDefault(addr => addr != IntPtr.Zero);
+                }
+                .Select(_process.Scan).FirstOrDefault(addr => addr != IntPtr.Zero);
 
-            if (addr_base == IntPtr.Zero)
-                return false;
+                if (addr_base == IntPtr.Zero)
+                    return false;
+            }
         }
 
         if (_process.Read(addr_base, out IntPtr ptr))
