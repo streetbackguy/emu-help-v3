d8e16056acc77a250e1044c64da1e78d24caa128
streetbackguy <65618875+streetbackguy@users.noreply.github.com>
2025-09-01 17:26:23 +0100

    Delete Systems/SNES directory

--- Systems/SNES/SNESEmulator.cs
+++
@@ -1,9 +0,0 @@
-﻿using EmuHelp.HelperBase;
-using System;
-
-namespace EmuHelp.Systems.SNES;
-
-public abstract class SNESEmulator : Emulator
-{
-    public IntPtr RamBase { get; protected set; }
-}
